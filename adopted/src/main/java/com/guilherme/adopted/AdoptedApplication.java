eea17298b69181af107864753756e7d6bf6779b1
gui.soares28 <gui.soares2899@gmail.com>
2026-01-26 16:49:20 -0300

    Initializing system development

---
+++ adopted/src/main/java/com/guilherme/adopted/AdoptedApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.adopted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptedApplication.class, args);
+	}
+
+}
