ae548e543f206872d427b535477f3a44f517e3e7
gui.soares28 <gui.soares2899@gmail.com>
2026-01-27 11:33:18 -0300

    chore(config): creation of configuration beans

--- adopted/src/main/java/com/guilherme/adopted/Security/SecurityConfig.java
+++ adopted/src/main/java/com/guilherme/adopted/Security/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.guilherme.adopted.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,8 +25,21 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**", "/h2-console").permitAll()
                 .anyRequest().authenticated()
-            );
+                
+            )
+            .headers(headers -> headers.frameOptions(frame -> frame.disable()));
             return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
