51188137992445f553d3465b66a6621afbaae0b7
gui.soares28 <gui.soares2899@gmail.com>
2026-01-27 16:31:40 -0300

    Creation of our userService contract

---
+++ adopted/src/main/java/com/guilherme/adopted/interfaces/IUserService.java
@@ -0,0 +1,21 @@
+package com.guilherme.adopted.interfaces;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.guilherme.adopted.dtos.UserRequestDto;
+import com.guilherme.adopted.dtos.UserResponseDto;
+import com.guilherme.adopted.dtos.UserUpdateDto;
+
+public interface IUserService {
+
+    UserResponseDto create(UserRequestDto userRequestDto);
+
+    List<UserResponseDto> getAllUsers();
+
+    UserResponseDto update(UserUpdateDto userUpdateDto);
+
+    UserResponseDto deleteById(UUID id);
+
+    UserResponseDto deleteByEmail(String email);
+}
