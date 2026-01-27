6bcfc3c03d3dfdcebae2047f53588fd53cd4fe01
gui.soares28 <gui.soares2899@gmail.com>
2026-01-27 14:22:23 -0300

    feat(Interface): inserting the getEmail function into the contract

--- adopted/src/main/java/com/guilherme/adopted/interfaces/ITokenService.java
+++ adopted/src/main/java/com/guilherme/adopted/interfaces/ITokenService.java
@@ -1,10 +1,11 @@
 package com.guilherme.adopted.interfaces;
-
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 public interface ITokenService {
 
-    String generatedToken();
+    String generatedToken(String email);
 
     DecodedJWT verifyToken(String token);
+
+    String getEmail(String token);
 }
